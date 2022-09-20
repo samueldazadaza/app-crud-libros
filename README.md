34573eb04776e6c9f687683e6781530345d32ad5
sam <samueldazadaza@gmail.com>
2022-09-20 12:09:05 -0500

    creacion plantilla inicial con spring initiallizer

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# app-crud-libros
+# Proyecto dedicado a la administracion de stock en una tienda.
+
+Creado en curso MINTIC desarrollo web -- UNIVERSIDAD UTP
---
+++ stock_tienda/src/main/java/com/proyecto/stock_tienda/StockTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.stock_tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTiendaApplication.class, args);
+	}
+
+}
