4dda34e917dd25583172264fc7ebf67f167c8842
Mirzo10 <105396482+Mirzo10@users.noreply.github.com>
2022-10-08 18:58:57 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,78 +1,112 @@
-var selectedRow = null
+consultarEstudiantes();
 
-function onFormSubmit(e) {
-	event.preventDefault();
-        var formData = readFormData();
-        if (selectedRow == null){
-            insertNewRecord(formData);
-		}
-        else{
-            updateRecord(formData);
-		}
-        resetForm();    
+async function consultarEstudiantes() {
+  var opciones = {
+    method: "GET",
+  };
+  var respuesta = await fetch(
+    "http://ee33-186-29-120-221.ngrok.io/api/v1/students", //todo cambiar url 
+    opciones
+  );
+  var datos = await respuesta.json();
+  console.log(datos);
+
+  document.getElementById("tablebody").innerHTML = ""; //Limpia la tabla antes de que se vuelva a llenar
+
+  for (i = 0; i < datos.results.length; i++) { //cambiar results (lo que aparece en la consola)
+    insertNewRecord(datos.results[i]); 
+  }
+  //InsertNewRecord()
 }
 
-//Retrieve the data
-function readFormData() {
-    var formData = {};
-    formData["id"] = document.getElementById("id").value;
-    formData["nombre"] = document.getElementById("nombre").value;
-    formData["autor"] = document.getElementById("autor").value;
-    formData["genero"] = document.getElementById("genero").value;
-    formData["cantidad"] = document.getElementById("cantidad").value;
-    return formData;
+async function registrarEstudiante() {
+  // Obtengo valor de la caja de texto
+  // Todo agregar getElementby para todos los campos y el JSON
+  var nombre = document.getElementById("nombre").value;
+  var id = document.getElementById("id").value;
+
+  var mijson = {
+    name: nombre, //todo cambiar las llaves de acuerdo al backend
+  };
+
+  if (id) {
+    // Configuaracion de opciones
+    var opciones = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(mijson),
+    };
+
+    await fetch(
+      `http://ee33-186-29-120-221.ngrok.io/api/v1/students/${id}/`,
+      opciones
+    );
+
+  } else {
+    // Configuaracion de opciones
+    var opciones = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(mijson),
+    };
+
+    var respuesta = await fetch(
+      "http://ee33-186-29-120-221.ngrok.io/api/v1/students/",
+      opciones
+    );
+    await respuesta.json();
+  }
+  consultarEstudiantes();
 }
 
 //Insert the data
 function insertNewRecord(data) {
-    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-		cell1.innerHTML = data.id;
-    cell2 = newRow.insertCell(1);
-		cell2.innerHTML = data.nombre;
-    cell3 = newRow.insertCell(2);
-		cell3.innerHTML = data.autor;
-    cell4 = newRow.insertCell(3);
-		cell4.innerHTML = data.genero;
-    cell5 = newRow.insertCell(4);
-		cell5.innerHTML = data.cantidad;
-    cell6 = newRow.insertCell(5);
-        cell6.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+  var table = document
+    .getElementById("storeList")
+    .getElementsByTagName("tbody")[0];
+  var newRow = table.insertRow(table.length);
+  cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.id;
+  cell2 = newRow.insertCell(1);
+  cell2.innerHTML = data.name; //todo nombre
+  cell3 = newRow.insertCell(2);
+  cell3.innerHTML = data.autor; 
+  cell4 = newRow.insertCell(3);
+  cell4.innerHTML = data.genero;
+  cell5 = newRow.insertCell(4);
+  cell5.innerHTML = data.cantidad;
+  cell6 = newRow.insertCell(5);
+  cell6.innerHTML = `<button onClick="editarEstudiante(${data.id})">Edit</button> <button onClick="borrarEstudiante(${data.id})">Delete</button>`;
 }
 
-//Edit the data
-function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("id").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("nombre").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("autor").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("genero").value = selectedRow.cells[3].innerHTML;
-    document.getElementById("cantidad").value = selectedRow.cells[4].innerHTML;
-}
-function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.id;
-    selectedRow.cells[1].innerHTML = formData.nombre;
-    selectedRow.cells[2].innerHTML = formData.autor;
-    selectedRow.cells[3].innerHTML = formData.genero;
-    selectedRow.cells[4].innerHTML = formData.cantidad;
+async function borrarEstudiante(id) {
+  // Configuaracion de opciones
+  var opciones = {
+    method: "DELETE",
+  };
+
+  await fetch(
+    `http://ee33-186-29-120-221.ngrok.io/api/v1/students/${id}/`,
+    opciones
+  );
+  consultarEstudiantes();
 }
 
-//Delete the data
-function onDelete(td) {
-    if (confirm('¿Quieres borrar éste libro?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById('storeList').deleteRow(row.rowIndex);
-        resetForm();
-    }
-}
+async function editarEstudiante(id) {
+  var opciones = {
+    method: "GET",
+  };
+  var respuesta = await fetch(
+    `http://ee33-186-29-120-221.ngrok.io/api/v1/students/${id}/`,
+    opciones
+  );
+  var datos = await respuesta.json();
+  console.log(datos);
 
-//Reset the data
-function resetForm() {
-    document.getElementById("id").value = '';
-    document.getElementById("nombre").value = '';
-    document.getElementById("autor").value = '';
-    document.getElementById("genero").value = '';
-    document.getElementById("cantidad").value = '';
-    selectedRow = null;
+  document.getElementById("nombre").value = datos.name; //cambiar a nombre de los del back
+  document.getElementById("id").value = datos.id; //cambiar a nombre
 }
